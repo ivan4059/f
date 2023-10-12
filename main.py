c8c187e7f007325b34c0b98b0de5c4ac2cdaf2fa
ivan4059 <145197246+ivan4059@users.noreply.github.com>
2023-10-12 22:06:35 +0200

    creato 1 bot
    che fa testa o croce con sto comando /testa_o_croce          con discord

--- main.py
+++ main.py
@@ -1,1 +1,49 @@
-in python mi crei un codice che manda questo sticker 👋 quando scrivo ciao
+import discord
+
+import random
+
+ 
+
+intents = discord.Intents.default()
+
+intents.typing = False
+
+intents.presences = False
+
+ 
+
+client = discord.Client(intents=intents)
+
+ 
+
+@client.event
+
+async def on_ready():
+
+    print(f'Connesso come {client.user.name}')
+
+ 
+
+@client.event
+
+async def on_message(message):
+
+    if message.author == client.user:
+
+        return
+
+ 
+
+    if message.content.startswith('/testa_o_croce'):
+
+        risultato = random.choice(['Testa', 'Croce'])
+
+        await message.channel.send(f'Hai lanciato una moneta e è uscito: {risultato}')
+
+ 
+
+# Inserisci il tuo token del bot Discord qui
+
+TOKEN ='MTE1NzYxOTY4NDkyMTg1MjAxNQ.G_tw-Q.JipNFgCMJahNF6qA7RKFWjcSOCRAzzObxyOlK4'
+
+client.run(TOKEN) 
